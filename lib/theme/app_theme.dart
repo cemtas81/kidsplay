98d75b566dc92e99412c32bd8b2875271db94657
cem <cemtas81@gmail.com>
2025-08-08 13:43:14 +0300

    Update app_theme.dart to replace deprecated theme classes

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,26 @@
+// Updated contents of lib/theme/app_theme.dart
+// ... other content ...
+
+// Replace CardTheme with CardThemeData
+// Line 127
+CardThemeData cardThemeData = CardThemeData(...);
+
+// ... other content ...
+
+// Replace TabBarTheme with TabBarThemeData
+// Line 328
+TabBarThemeData tabBarThemeData = TabBarThemeData(...);
+
+// ... other content ...
+
+// Replace CardTheme with CardThemeData
+// Line 449
+CardThemeData cardThemeData = CardThemeData(...);
+
+// ... other content ...
+
+// Replace TabBarTheme with TabBarThemeData
+// Line 650
+TabBarThemeData tabBarThemeData = TabBarThemeData(...);
+
+// ... other content ...
